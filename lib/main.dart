29e9b15af534dc85014624cac150991c1d8aed10
varshachavda <varsha@prepseed.com>
2024-03-09 21:56:53 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'dart:isolate';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    // Future.microtask(() async {
+    //    runWithoutIsolate();
+    // });
+    super.initState();
+  }
+
+  runWithoutIsolate()  {
+    for (int i = 0; i < 40000; i++) {
+      print(i);
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()  {
+          //  runWithoutIsolate();
+          runwithIsolate();
+          _incrementCounter();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+  runwithIsolate() async {
+    final ReceivePort receivePort = ReceivePort();
+    try {
+      await Isolate.spawn(runHeavyTaskWithIsolate, [receivePort.sendPort,4000]);
+    } on Object{
+      receivePort.close();
+    }
+  }
+
+runHeavyTaskWithIsolate(List<dynamic> args){
+  SendPort resultPort = args[0];
+  int value =0;
+  for(var i=0;i<args[1];i++){
+    value += i;
+    print('isolate $i');
+  }
+  Isolate.exit(resultPort,value);
+}
+  
